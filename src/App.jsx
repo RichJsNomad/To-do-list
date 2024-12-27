5dfd281a91b1bd6fcde8e5d2555aa9b593701932
Aleks <karaeeeani@gmail.com>
2024-12-27 10:20:01 +0200

    Adding styles and making sure all components work properly, importing everything to app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
-
-return <></>;
+import Header from "./components/Header";
+import Tabs from "./components/Tabs";
+import TodoCard from "./components/TodoCard";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
 
 export default App;
